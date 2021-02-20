783a65bf600ccaa7323dbc57d658386b3569e377
Marie-Louise <mariemuteteyimana@gmail.com>
2021-02-21 01:30:29 +0300

    1st Assignment

---
+++ src/1st Assignment.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val name="marieLouise"
+    println(name)
+    var age=40
+    println(age)
+    var weight=32.4343434
+    println(weight)
+    val citizen= "false"
+    println(citizen)
+    val student=arrayOf("marieLouise",40,32.434343,"false")
+    print(student
+    )
+}
